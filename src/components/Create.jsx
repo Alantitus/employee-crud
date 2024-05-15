25f3cb2ac4e58ec4f75c274e6130c448dddd6e44
Alantitus <alantitus89@gmail.com>
2024-05-15 11:17:52 +0530

    first commit

---
+++ src/components/Create.jsx
@@ -0,0 +1,67 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Dropdown } from 'react-bootstrap'
+
+
+function Create() {
+    const [values,setValues]=useState({
+        id:'',
+        username:'',
+        email:'',
+        status:'',
+    })
+    const navigate= useNavigate()
+    const handleSubmit=(event)=>{
+        event.preventDefault();
+        axios.post('http://localhost:3000/users',values)
+        .then(res=>{
+            console.log(res)
+            navigate('/')
+        }
+            
+        )
+        .catch(err=>console.log(err))
+    }
+  return (
+    <div style={{height:'100vh'}} className='d-flex w-100 justify-content-center align-items-center bg-light'>
+    <div className='w-50 border bg-white shadow px-5 pt-3 pb-5 rounded'>
+     <h1>Add a Employee</h1>
+     <form onSubmit={handleSubmit}>
+     <div className='mb-2'>
+         <label htmlFor="name">Id:</label>
+         <input onChange={e => setValues({...values,id: e.target.value})} type="text" name='id'
+         className='form-control' placeholder='Enter your Id' />
+       </div>
+       <div className='mb-2'>
+         <label htmlFor="name">Name:</label>
+         <input onChange={e => setValues({...values,username: e.target.value})} type="text" name='username'
+         className='form-control' placeholder='Enter your name' />
+       </div>
+
+       <div className='mb-2'>
+         <label htmlFor="email">Email:</label>
+         <input onChange={e => setValues({...values,email: e.target.value})} type="email" name='email'
+         className='form-control' placeholder='Enter your email' />
+       </div>
+
+
+       <div className='mb-2'>
+         <label htmlFor="phone">Status:</label>
+         <select className='btn btn-primary' onChange={e => setValues({...values,status: e.target.value})}   >
+        <option className='btn btn-success'>Active</option>
+        <option className='btn btn-danger'>Inactive</option>
+         </select>
+    </div>
+       <button onClick={handleSubmit} className='btn btn-success'>Submit</button>
+
+       <Link to="/" className='btn btn-primary ms-3'>Back</Link>
+
+
+     </form>
+   </div>
+  </div>
+  )
+}
+
+export default Create
---
+++ src/components/Update.jsx
@@ -0,0 +1,66 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import Dropdown from 'react-bootstrap/Dropdown';
+
+function Update() {
+    const {id}=useParams()
+    const [values,setValues] = useState({
+        username: '',
+        email: '',
+        status: '',
+      })
+    
+      const navigate=useNavigate();
+    
+    useEffect(()=>{
+        axios.get('http://localhost:3000/users/'+id)
+        .then(res=>{
+            setValues(res.data)
+        })
+        .catch(err=>console.log(err))
+    },[])
+
+    const handleUpdate=(event)=>{
+        event.preventDefault()
+        axios.put('http://localhost:3000/users/'+id,values)
+        navigate('/')
+    }
+  return (
+    <div style={{height:'100vh'}} className='d-flex w-100 justify-content-center align-items-center bg-light'>
+     <div className='w-50 border bg-white shadow px-5 pt-3 pb-5 rounded'>
+      <h1>Update Employee</h1>
+      <form onSubmit={handleUpdate}>
+        <div className='mb-2'>
+          <label htmlFor="name">Name:</label>
+          <input onChange={e => setValues({...values,name: e.target.value})} value={values.username}  type="text" name='name'
+          className='form-control' placeholder='Enter your name' />
+        </div>
+
+        <div className='mb-2'>
+          <label htmlFor="email">Email:</label>
+          <input onChange={e => setValues({...values,email: e.target.value})} value={values.email}   type="email" name='email'
+          className='form-control' placeholder='Enter your email' />
+        </div>
+
+
+        <div className='mb-2'>
+          <label htmlFor="phone">Status:</label>
+         <select className='btn btn-primary' onChange={e => setValues({...values,status: e.target.value})} value={values.status}  >
+        <option className='btn btn-success'>Active</option>
+        <option className='btn btn-danger'>Inactive</option>
+         </select>
+   
+      </div>
+        <button className='btn btn-success'>Update</button>
+
+        <Link to="/" className='btn btn-primary ms-3'>Back</Link>
+
+
+      </form>
+    </div>
+   </div>
+  )
+}
+
+export default Update
